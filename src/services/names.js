1606dc85fd5276b31d012bed1bcbbec0e9c1dccd
Lara Esquivel de Brito Santos <laraesquivelsantos@gmail.com>
2024-01-21 10:35:22 -0300

    Update names.js

--- src/services/names.js
+++ src/services/names.js
@@ -3,7 +3,7 @@ import auth from './auth.js'
 
 export default{
     getNames : (name) => {
-        let code = import.meta.env.VITE_GET_NAMES_CODE
+        let code = import.meta.env.VITEAZURE_GET_NAMES_CODE
         console.log(code)
         const url = `/getNames?code=${code}==&name=${name}`;
         return api.get(url,auth)

462976121f927ab529715df30e0075b722956699
laraesquivel <laraesquivelsantos@gmail.com>
2024-03-17 15:49:07 -0300

    test2

--- src/services/config.js
+++ src/services/config.js
@@ -1,10 +1,10 @@
 //Prod url
 
-//export const API_BASE_URL = "https://adam-serveless-babynames.vercel.app";
+export const API_BASE_URL = "https://adam-serveless-babynames.vercel.app";
 
 
 //Dev url
-export const API_BASE_URL = "https://adam-serveless-babynames-git-dev-adam-babys-projects.vercel.app/?vercelToolbarCode=jU7O7f-70w64OAX"
+//export const API_BASE_URL = "https://adam-serveless-babynames-git-dev-adam-babys-projects.vercel.app/?vercelToolbarCode=jU7O7f-70w64OAX"
 
 //LocalHost Url
 

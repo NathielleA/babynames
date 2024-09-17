7223cd8a934210064e0dc83872e44adf0bec7987
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-09-17 09:27:34 -0300

    Google Analytcs

--- src/main.js
+++ src/main.js
@@ -11,3 +11,13 @@ const app = createApp(App).use(store)
 app.use(router)
 //app.use(Buefy)
 app.mount('#app')
+
+// Configuração do Google Analytics
+router.afterEach((to) => {
+    if (typeof gtag !== 'undefined') {
+        gtag('config', 'G-1WSF5H7V0P', { page_path: to.path });
+    }
+});
+
+// Usa o router 
+app.use(router).mount('#app');

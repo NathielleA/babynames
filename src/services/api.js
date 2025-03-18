be7e8a58232cf7acf1340021b7305f91ce0c3dc9
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2025-03-18 14:47:54 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "https://adam-serveless-babynames.vercel.app",
-    timeout: 9000,
+    timeout: 15000,
     withCredentials: true, // Permite cookies e credenciais
     headers: {
         'Accept': 'application/json',

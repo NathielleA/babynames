c35fd866c1d1a2ec390d354c2212dfa13c26fc93
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2025-04-22 15:11:42 -0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -220,8 +220,9 @@ export default createStore({
     },
 
     async fetchUserAssignature({ commit }) {
+      console.log("eu aqui estou")
       try {
-        console.log("eu aqui estou")
+        
         let userId = this.state.userToken;
         let response = await users.getUserId(userId); // Faz a requisição ao servidor
         let assignature = response.data.assignature; // Obtém a assinatura do usuário

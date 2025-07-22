836691196c12a1786187af9faec26a0dc7e2edd3
Nathielle Alves <nalves@ecomp.uefs.br>
2025-07-22 10:23:24 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -13,59 +13,58 @@ function setCookie(name, value, days) {
   const expires = `expires=${d.toUTCString()}`;
   document.cookie = `${name}=${value}; ${expires}; path=/`;
 }
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      beforeEnter: (to, from, next) => {
+      name : 'home',
+      beforeEnter : (to,from,next) =>{
         const EXISTINGVARIANT = getCookie('variant');
-        if (!EXISTINGVARIANT) {
-          const variant = Math.random() < 0.5 ? 'A' : 'B';
-          setCookie('variant', variant, 7);
-          next({ name: variant === 'A' ? 'InterfaceA' : 'InterfaceB' });
-        } else {
+        if (!EXISTINGVARIANT){
+            const variant = Math.random() < 0.5 ? 'A' : 'B'
+            setCookie('variant', variant,7)
+            next({name:variant == 'A' ? 'InterfaceA' : 'InterfaceB'})
+            
+        }
+        else {
           next({ name: EXISTINGVARIANT === 'A' ? 'InterfaceA' : 'InterfaceB' });
         }
+  
       },
-      component: () => import("../views/HomeView.vue")
+      component : () => import("../views/HomeView.vue")
+      
     },
     {
       path: '/interface-a',
       name: 'InterfaceA',
-      component: () => import('../views/HomeView.vue'),
+      component: ()=> import('../views/HomeView.vue'),
     },
     {
       path: '/interface-b',
       name: 'InterfaceB',
-      component: () => import('../views/HomeViewB.vue'),
+      component: ()=>import('../views/HomeViewB.vue'),
     },
     {
-      path: '/search',
+      path: '/search/',
       name: 'search',
-      beforeEnter: (to, from, next) => {
-        const variant = getCookie('variant');
-        if (variant === 'B') {
-          next({ name: 'SearchB' });
-        } else {
-          next();
-        }
-      },
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
       component: () => import('../views/SearchView.vue')
     },
-    {
-      path: '/search-b',
-      name: 'SearchB',
-      component: () => import('../views/SearchViewB.vue')
-    },
+
     {
       path: '/phrase-recommendations',
       name: 'RecommendationPage',
       component: () => import('../views/PhrasesView.vue')
+    },
+    
+    {
+
     }
   ]
 })
+
 
 export default router

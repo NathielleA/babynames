4a7e723fe29ead16ae51a39654907772d24e3540
Nathielle Alves <nalves@ecomp.uefs.br>
2025-07-24 15:37:26 -0300

    Update PhrasesNotification.vue

--- src/components/home/PhrasesNotification.vue
+++ src/components/home/PhrasesNotification.vue
@@ -98,7 +98,7 @@ export default {
     goToPhraseRecommendations() {
       if (this.phrase && this.phrase.associedNames) {
         this.$store.commit('setIsPhraseSearch', true);  // muda o header
-        this.$store.commit('setSelectedPhrase', this.phrase);  // guarda a frase
+        this.$store.commit('setName', this.phrase.Frase);
         this.$store.commit('setRecommendedNames', this.phrase.associedNames);
 
         this.$router.push('/search');
